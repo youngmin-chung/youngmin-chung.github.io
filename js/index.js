d8c29efb7c7ec9f0f7cc573fdab6c9663be8f893
Youngmin Chung <y_chung2@fanshaweonline.ca>
2020-09-01 02:41:04 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,71 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
+const navPortfolio = document.querySelectorAll(".portfolio__item");
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Youngmin", "a developer"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        if (!cursorSpan.classList.contains("typing"))
+            cursorSpan.classList.add("typing");
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(
+            charIndex
+        );
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        cursorSpan.classList.remove("typing");
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        if (!cursorSpan.classList.contains("typing"))
+            cursorSpan.classList.add("typing");
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(
+            0,
+            charIndex - 1
+        );
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        cursorSpan.classList.remove("typing");
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    // On DOM Load initiate the effect
+    if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+navToggle.addEventListener("click", () => {
+    document.body.classList.toggle("nav-open");
+});
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        document.body.classList.remove("nav-open");
+    });
+});
+
+// Scroll to top
+$("#to-top").click(function () {
+    $("html, body").animate(
+        {
+            scrollTop: 0,
+        },
+        500
+    );
+});
